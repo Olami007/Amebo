daa31b1b612abf878251cae07941a478e81f3fca
Olami007 <oladimejibells@gmail.com>
2023-07-23 19:09:39 +0100

    SessionStatus correctly updated as a client component

--- src/components/SessionStatus/Sess.jsx
+++ src/components/SessionStatus/Sess.jsx
@@ -1,20 +1,23 @@
-// "use client";
+"use client";
 
-// import { useSession } from "next-auth/react";
-// import { useRouter } from "next/navigation";
-// import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-// export const checkStatus = () => {
-//   const session = useSession();
-//   const router = useRouter();
-//   useEffect(() => {
-//     console.log(session);
-//     if (session.status === "unauthenticated") {
-//       router.push("/");
-//     }
-//   }, [session.status, router]);
+export const CheckStatus = ({ children }) => {
+  const session = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    console.log(session);
+    if (session.status === "unauthenticated") {
+      router.push("/");
+    }
+  }, [session.status, router]);
 
-//   if (session.status === "loading") {
-//     return <p>Loading...</p>;
-//   }
-// };
+  if (session.status === "loading") {
+    return <p>Loading...</p>;
+  }
+  if (session.status === "authenticated") {
+    return <>{children}</>;
+  }
+};
