a0f752b4b47fc15ccf06243ac94728ddcaaadc3b
Olami007 <oladimejibells@gmail.com>
2023-07-24 14:11:35 +0100

    User model updated to contain more fields

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,9 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema(
   {
-    name: { type: String, unique: true, required: true },
+    username: { type: String, unique: true, required: true },
+    firstName: { type: String, unique: true, required: true },
+    lastName: { type: String, unique: true, required: true },
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
   },
