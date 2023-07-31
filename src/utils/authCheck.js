8bf320acecccfe4b3bce917c8b78d96843a5b6fa
Olami007 <oladimejibells@gmail.com>
2023-07-31 21:45:04 +0100

    auth check () cretaed to be used in server components

---
+++ src/utils/authCheck.js
@@ -0,0 +1,22 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function redirectToSignInIfNoToken(Component) {
+  return function ProtectedRoute({ ...props }) {
+    const session = useSession();
+    const router = useRouter();
+    useEffect(() => {
+      if (session.status === "unauthenticated") {
+        router.push("/auth/login");
+      }
+      //   if (session.status === "loading") {
+      //     router.push("/auth/login");
+      //   }
+    }, [router, session]);
+
+    return <Component {...props} />;
+  };
+}
