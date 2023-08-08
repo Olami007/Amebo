f279d9a1a719a9f0cd96546f663502e75ce04fd0
Olami007 <oladimejibells@gmail.com>
2023-08-08 21:03:14 +0100

    FetchSingleData component created

--- src/components/FetchSingleData/FetchSingleData.jsx
+++ src/components/FetchSingleData/FetchSingleData.jsx
@@ -1,15 +1,3 @@
-// export const getSingleData = async (id) => {
-//   const res = await fetch(`${process.env.BASE_URL}/api/feed/${id}`, {
-//     next: { revalidate: 10 },
-//   });
-
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch data");
-//   }
-//   const data = await res.json();
-//   return data;
-// };
-
 export const getSingleData = async (id) => {
   const res = await fetch(`${process.env.BASE_URL}/api/feed/${id}`, {
     cache: "no-store",
@@ -21,3 +9,14 @@ export const getSingleData = async (id) => {
   const data = await res.json();
   return data;
 };
+// export const getUserFeeds = async (userUsername) => {
+//   const res = await fetch(`${process.env.BASE_URL}/api/feed/${userUsername}`, {
+//     cache: "no-store",
+//   });
+
+//   if (!res.ok) {
+//     return notFound();
+//   }
+//   const data = await res.json();
+//   return data;
+// };
