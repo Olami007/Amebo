a0c89e648bf44432ef4e6a13db51963ac0a64acb
Olami007 <oladimejibells@gmail.com>
2023-08-10 14:47:41 +0100

    PopUp component created

---
+++ src/components/PopUp.jsx
@@ -0,0 +1,9 @@
+// import React from "react";
+// import Popup from "reactjs-popup";
+// import "reactjs-popup/dist/index.css";
+
+// export const PopUp = () => (
+//   <Popup trigger={<button> Trigger</button>} position="right center">
+//     <div className="text-blue-400">Popup content here !!</div>
+//   </Popup>
+// );
