1c7cda89b9081552c43a17522bdad9862fa10270
Olami007 <oladimejibells@gmail.com>
2023-08-10 15:11:01 +0100

    dashboard pos page converted to server rendering

--- src/app/dashboard/posts/page.jsx
+++ src/app/dashboard/posts/page.jsx
@@ -1,66 +1,17 @@
-"use client";
-
-import { CreatedAt } from "@/components/CreatedAt/CreatedAt";
-import LikeButton from "@/components/LikeButton/LikeButton";
-import Navbar from "@/components/Navbar/Navbar";
-import RetweetButton from "@/components/RetweetButton/RetweetButton";
-import Spinner from "@/components/Spinner";
-import { useSession } from "next-auth/react";
-import Link from "next/link";
 import React from "react";
-import useSWR from "swr";
+import Post from "./Post";
+import { getServerSession } from "next-auth";
+import { handler } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-const Page = () => {
-  const session = useSession();
+const Page = async () => {
+  const session = await getServerSession(handler);
 
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-  const { data, mutate, error, isLoading } = useSWR(
-    `http://localhost:3000/api/userfeed?username=${session?.data?._doc.username}`,
-    fetcher
-  );
-
-  console.log(data);
-  return (
-    <>
-      <Navbar />
-      <div className="text-center py-4">My Amebo Feeds</div>
-
-      <div>
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          data?.map((feed) => (
-            <div className=" p-8 border-y-2" key={feed?._id}>
-              <Link href={`/feeds/${feed._id}`}>
-                <div className="flex justify-between align-center">
-                  <div>
-                    <div className="pb-2">
-                      <Link href={`/userfeeds/${feed.userUsername}`}>
-                        <span>{feed.userFirstName}</span>
-                        <span className="pl-4">{`@${feed.userUsername}`}</span>
-                      </Link>
-                    </div>
-                    <h2>{feed?.content}</h2>
-                  </div>
-                  <small>{CreatedAt(feed?.createdAt)}</small>
-                </div>
-              </Link>
-
-              <span className="flex ">
-                {" "}
-                <LikeButton />
-                <RetweetButton />
-              </span>
-            </div>
-          ))
-        )}
-      </div>
-      <Link className="fixed bottom-8 right-8 text-7xl" href="/feeds/compose">
-        +
-      </Link>
-    </>
-  );
+  if (!session) {
+    return redirect("/auth/login?login=unauthenticatedUser");
+  }
+  console.log(session, "serversession");
+  return <Post />;
 };
 
 export default Page;
