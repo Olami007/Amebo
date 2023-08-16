fddc026d78035fa7984d1048e868be66b42a3dc8
Olami007 <oladimejibells@gmail.com>
2023-08-16 11:23:36 +0100

    feed [id]  page updated

--- src/app/feeds/[id]/page.jsx
+++ src/app/feeds/[id]/page.jsx
@@ -3,6 +3,7 @@ import { getSingleData } from "@/components/FetchSingleData/FetchSingleData";
 import LikeButton from "@/components/LikeButton/LikeButton";
 import Navbar from "@/components/Navbar/Navbar";
 import RetweetButton from "@/components/RetweetButton/RetweetButton";
+import Link from "next/link";
 import React from "react";
 
 const page = async ({ params }) => {
@@ -17,10 +18,12 @@ const page = async ({ params }) => {
         {/* <h1>My Post</h1> */}
         <div className="flex justify-between align-center">
           <div>
-            <div className="pb-2">
-              <span>{feed.userFirstName}</span>
-              <span className="pl-4">{`@${feed.userUsername}`}</span>
-            </div>
+            <Link href={`/userfeeds/${feed.userUsername}`}>
+              <div className="pb-2">
+                <span>{feed.userFirstName}</span>
+                <span className="pl-4">{`@${feed.userUsername}`}</span>
+              </div>
+            </Link>
             <h2>{feed?.content}</h2>
           </div>
           <small>{CreatedAt(feed?.createdAt)}</small>
